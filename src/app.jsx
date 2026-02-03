a7be140cec8bf0bf317f89271d459b807e784307
matéo! <6matbub@gmail.com>
2026-02-03 12:14:49 -0800

    add vercel analytics

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-// import {BrowserRouter as Router} from 'react-router-dom';
-import Routes from './comps/routes_';
+import {Analytics} from '@vercel/analytics/next';
 import Demo from './pages/landing_page';
 import MobileWarn from './pages/mobile_warning';
 import './styles/global.css';
@@ -9,7 +8,12 @@ import Window from './comps/helpers/handleWindow';
 
 const App = () => {
   const {width} = Window();
-  return <>{width > 576 ? <Demo /> : <MobileWarn />}</>;
+  return (
+    <>
+      {width > 576 ? <Demo /> : <MobileWarn />}
+      <Analytics />
+    </>
+  );
 };
 
 export default App;
