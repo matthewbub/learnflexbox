50a134bdefade891e26251f6866808d5c0cdb5bd
matéo! <6matbub@gmail.com>
2026-02-03 11:31:23 -0800

    rm unused components and screens

--- src/comps/routes_.jsx
+++ src/comps/routes_.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import {Switch, Route} from 'react-router-dom';
-//pages
 import Demo from '../pages/landing_page';
-import ComingSoon from './coming-soon_';
 import NotFound from '../pages/not-found_page';
-import About from '../pages/about_page';
-import License from '../pages/license';
 
 const Routes = () => {
   return (
@@ -15,13 +11,6 @@ const Routes = () => {
         path={['/', '/flexbox', '/flexbox-generator', '/home']}
         render={() => <Demo />}
       />
-      <Route
-        exact
-        path={['/assessments', '/challenges', '/support']}
-        render={() => <ComingSoon />}
-      />
-      <Route exact path={'/about'} render={() => <About />} />
-      <Route exact path={'/license'} render={() => <License />} />
       <Route render={() => <NotFound />} />
     </Switch>
   );
